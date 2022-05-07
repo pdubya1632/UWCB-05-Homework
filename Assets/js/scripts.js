ee32a1f1b6a7d285caa42b6d6dcb329cb8fe3222
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-07 13:46:09 -0700

    swap textarea for input and verify that localstorage still works correctly

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,49 +1,49 @@
 $(function () {
-    // show current day in header
-    $("#currentDay").html(dayjs().format("dddd, MMMM D"));
+  // show current day in header
+  $("#currentDay").html(dayjs().format("dddd, MMMM D"));
 
-    // insert all previous entries
-    for (var i = 0; i < localStorage.length; i++) {
-        
-        // grab time and entry from localStorage
-        let entryHour = localStorage.key(i);
-        let entryText = localStorage.getItem(entryHour);
+  // insert all previous entries
+  for (var i = 0; i < localStorage.length; i++) {
 
-        // find time row and set entry in input
-        $("#" + entryHour)
-            .find("input[type=text]")
-            .val(entryText);
-    };
+    // grab time and entry from localStorage
+    let entryHour = localStorage.key(i);
+    let entryText = localStorage.getItem(entryHour);
 
-    // grab current hour
-    const currentHour = dayjs().format('hA');
-    //const currentHour = dayjs().add(10, "h").format("HA");
-    console.log("Current hour: " + currentHour);
+    // find time row and set entry in input
+    $("#" + entryHour)
+      .find("textarea")
+      .val(entryText);
 
-    // if current hour exists in calendar
-    if ($("#" + currentHour)) {
-        
-        // set current hour variable
-        const currentHourInput = $("#" + currentHour);
+  }
 
-        // give current house input a red bg
-        currentHourInput
-            .find("input[type=text]")
-            .css("background-color", "#50AD51");
+  // grab current hour
+  const currentHour = dayjs().format("hA");
+  console.log("Current hour: " + currentHour);
 
-        // make all inputs *after* get a green bg
-        currentHourInput
-            .nextAll("div")
-            .find("input[type=text]")
-            .css("background-color", "#6FAB70");
-    
-    }
+  // if current hour exists in calendar
+  if ($("#" + currentHour)) {
+    // set current hour variable
+    const currentHourInput = $("#" + currentHour);
 
+    // give current house input a red bg
+    currentHourInput.prevAll(".calendar div").find("textarea").addClass("past");
+
+    // give current hour textarea a red bg
+    currentHourInput.find("textarea").addClass("present");
+
+    // make all textarea *after* get a green bg
+    currentHourInput.nextAll("div").find("textarea").addClass("future");
+  }
 });
 
 // log new day entry upon hitting save
 $("button").click(function () {
-  let key = $(this).closest("div").attr("id");
-  let entry = $(this).prev().val();
-  localStorage.setItem(key, JSON.stringify(entry));
+
+  var entry = $.trim($(this).prev().val());
+
+  if (entry != "") {
+    let key = $(this).closest("div").attr("id");
+    localStorage.setItem(key, entry);
+  }
+
 });
