72e48886954d0c0941b5b8e2ea976b8f26d2db0b
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-07 01:03:04 -0700

    retrieve current hour and try to find that row on page

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,3 +1,22 @@
 
 // show current day in header
-document.getElementById("currentDay").innerHTML = dayjs().format('dddd, MMMM D');
+$("#currentDay").html(dayjs().format('dddd, MMMM D'));
+
+// color code time blocks
+// const currentHour = dayjs().format('hA');
+const currentHour = dayjs().add(9, 'h').format('Ha');
+if ($( "#" + currentHour )) {
+    $( "#" + currentHour ).find("input[type=text]").css('background-color', 'red');
+};
+
+// insert all previous entries
+for (var i = 0; i < localStorage.length; i++){
+    $('body').append(localStorage.getItem(localStorage.key(i)));
+};
+
+// log new day entry
+$( "button " ).click(function() {
+    let key = $(this).closest('div').attr('id');;
+    let entry = $(this).prev().val();
+    localStorage.setItem(key , entry);
+});
